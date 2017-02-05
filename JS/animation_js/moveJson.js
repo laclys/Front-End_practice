3affc465519f5c26de12e307a8687390747adebb
laclys <laclys@126.com>
2017-02-05 23:20:21 +0800

    fix bugs
    解决js动画同时进行上的一些bug。

--- JS/animation_js/moveJson.js
+++ JS/animation_js/moveJson.js
@@ -3,27 +3,26 @@ function getStyle(obj, name) {
 }
 
 function startMove(obj, json, fn) {
-    console.log(json.width);
-    console.log(json.height);
-    for (var attr in json) {
-        console.log(attr + '    ' + json[attr]);
-        clearInterval(obj.timer);
-        obj.timer = setInterval(function () {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        var flag = true;
+        for (var attr in json) {
+            // console.log(attr + '    ' + json[attr]);
             var flag = true;
             var icur = parseInt(getStyle(obj, attr));
             var speed = (json[attr] - icur) / 8;
             speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
             if (icur != json[attr]) {
                 flag = false;
-
                 obj.style[attr] = icur + speed + 'px';
             }
-            if(flag){
-                clearInterval(obj.timer);
-                if(fn){
-                    fn();
-                }
+
+        }
+        if (flag) {
+            clearInterval(obj.timer);
+            if (fn) {
+                fn();
             }
-        }, 30)
-    }
+        }
+    }, 30)
 }
