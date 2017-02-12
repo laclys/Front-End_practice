27fe3ca95f1d8901e77a909a33f54f1238eb6857
laclys <laclys@126.com>
2017-02-12 17:59:39 +0800

    add files

--- sass_web2/yunDao/js/index.js
+++ sass_web2/yunDao/js/index.js
@@ -1,10 +1,15 @@
-var oTab = document.getElementById('tabImg');
-var oTabImg = oTab.getElementsByTagName('img')[0];
-var clientW = document.documentElement.clientWidth;
-if (clientW <= 1234) {
-    clientW = 1234;
-    oTab.style.width = clientW + 'px';
-    oTabImg.style.width = clientW + 'px';
+
+window.onload=window.onresize = function () {
+    var oTab = document.getElementById('tabImg');
+    var oTabImg = oTab.getElementsByTagName('img')[0];
+    var clientW = document.documentElement.clientWidth;
+    var oService=document.getElementById('service');
+    if (clientW <= 1234) {
+        clientW = 1234;
+        oTab.style.width = clientW + 'px';
+        oTabImg.style.width = clientW + 'px';
+    }
+    oTabImg.style.height = clientW * (620 / 1903) + 'px';
+    oTab.style.height = clientW * (620 / 1903) + 'px';
+    oService.style.marginTop=oTabImg.style.height;
 }
-oTabImg.style.height = clientW * (620 / 1903) + 'px';
-oTab.style.height = clientW * (620 / 1903) + 'px';
