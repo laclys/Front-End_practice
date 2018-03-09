cb750f08a671318e17e08c702e8fb9cc48bb7b1a
Lac <laclys@126.com>
2018-03-09 18:28:53 +0800

    code style

--- node/toPic/index.js
+++ node/toPic/index.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
-
 const gm = require('gm')
-
 // test.jpg 600*600 1.jpg 140*140
-
-var _name = "China中文";
-
 gm('./test.jpg')
   .draw('image Over 460, 460, 140, 140 "./1.jpg"')
   .drawText(100, 100, '哇')
@@ -13,7 +8,6 @@ gm('./test.jpg')
   .font('./font/微软雅黑.ttf')
   .write(`./output/${Date.now()}.jpg`, function(err) {
     if (!err) {console.log('done')}else{
-      console.log(err.message || "出错了！");
+      console.log(err.message || "出错了！")
     }
-    
   })
--- node/toPic/test2.js
+++ node/toPic/test2.js
@@ -1,13 +1,8 @@
-const fs = require('fs')
-
 const gm = require('gm')
 
 // test.jpg 600*600 1.jpg 140*140
-
-
 gm('./test.jpg')
   .draw('image Over 460, 460, 140, 140 "./1.jpg"')
-  
-  .write(`./output/${Date.now()}.jpg`, function(err) {
+  .write(`./output/${Date.now()}.jpg`, function (err) {
     if (!err) {console.log('done')}
   })
