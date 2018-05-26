4ed29e0ff8853f29c2dc4fea3fd396ab8ca09ab6
Lac <laclys@126.com>
2018-05-26 19:21:50 +0800

    mpvue-demo

---
+++ mpvue-demo/mpvue-demo/src/components/TodoList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="mytodo" @keyup.enter="handleClick" />
+    <button @click="handleClick">添加</button>
+    <ul>
+      <li :class="{'done': todo.done}" @click="toggle(index)" v-for="(todo, index) in todos" :key="index">{{index + 1}}.{{todo.text}}</li>
+    </ul>
+    <p>{{remains}}/{{todos.length}}</p>
+    <button @click="handleClear">清空</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      mytodo: '',
+      todos: [
+        { text: '吃饭', done: false },
+        { text: '睡觉', done: false },
+        { text: '打豆豆', done: false }
+      ]
+    }
+  },
+  computed: {
+    remains () {
+      return this.todos.filter(v => !v.done).length
+    }
+  },
+  methods: {
+    handleClick () {
+      if (this.trim(this.mytodo) === '') return
+      this.todos.push({ text: this.mytodo, done: false })
+      this.mytodo = ''
+    },
+    toggle (i) {
+      this.todos[i].done = !this.todos[i].done
+    },
+    handleClear () {
+      this.todos = this.todos.filter(todo => !todo.done)
+    },
+    trim (str) {
+      return str.replace(/(^\s*)|(\s*$)/g, '')
+    }
+  }
+}
+</script>
+
+<style>
+ol,
+ul {
+  list-style: none;
+}
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+li {
+  text-align: center;
+}
+li.done {
+  text-decoration: line-through;
+}
+</style>
---
+++ mpvue-demo/mpvue-demo/src/pages/todolist/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>mpvue todolist</h1>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+
+export default {
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+  h1{
+    color: red;
+  }
+</style>
+
