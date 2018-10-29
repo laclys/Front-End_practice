ce956a479394c63e9fab7ee3a7d4d00b91af2ad4
Lac <laclys@126.com>
2018-10-29 22:47:51 +0800

    added

--- design-pattern/src/index.js
+++ design-pattern/src/index.js
@@ -1,26 +1,11 @@
-// 装饰器模式： 为对象添加新功能；不改变其原有的结构和功能
-class Circle {
-  draw() {
-    console.log('draw~')
-  }
+// ES7 decorator
+@testDec
+class Demo {
+
 }
 
-class Decorator {
-  constructor(circle) {
-    this.circle = circle
-  }
-  draw() {
-    this.circle.draw()
-    this.setRedBorder(circle)
-  }
-  setRedBorder(circle) {
-    console.log('set red border')
-  }
+function testDec(target) {
+  target.isDec = true
 }
 
-// test
-let circle = new Circle()
-circle.draw()
-console.log('-----')
-let dec = new Decorator(circle)
-dec.draw()
+alert(Demo.isDec)
