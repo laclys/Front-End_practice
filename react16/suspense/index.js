6701d72fa7b48db126c6bcba1105198912108700
Lac <laclys@126.com>
2019-01-16 00:00:20 +0800

    added

---
+++ react16/suspense/index.js
@@ -0,0 +1,30 @@
+import React, { Suspense, lazy } from 'react'
+
+const LazyComp = lazy(() => import('./lazy.js'))
+
+let data = ''
+let promise = ''
+function requestData() {
+  if (data) return data
+  if (promise) throw promise
+  promise = new Promise(resolve => {
+    setTimeout(() => {
+      data = 'Data resolved'
+      resolve()
+    }, 2000)
+  })
+  throw promise
+}
+
+function SuspenseComp() {
+  const data = requestData()
+
+  return <p>{data}</p>
+}
+
+export default () => (
+  <Suspense fallback="loading data">
+    <SuspenseComp />
+    <LazyComp />
+  </Suspense>
+)
