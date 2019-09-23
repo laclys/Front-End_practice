9852ab20aef66debbf0d4b669d931463799a041a
Lac <laclys@126.com>
2019-09-23 19:06:54 +0800

    added

---
+++ wx-charts/折线图-demo/pages/index/index.js
@@ -0,0 +1,60 @@
+var wxCharts = require('../../utils/wxcharts.js')
+var lineChart = null
+
+Page({
+  data: {},
+  onLoad: function () {
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    var simulationData = this.createSimulationData();
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: simulationData.categories,
+      animation: true,
+      // background: '#f5f5f5',
+      series: [{
+        name: '成交量',
+        data: simulationData.data,
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: '金额 (万元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    })
+  },
+  createSimulationData: function () {
+    var categories = [];
+    var data = [];
+    for (var i = 0; i < 10; i++) {
+      categories.push('2019-' + (i + 1));
+      data.push(Math.random() * 10);
+    }
+    return {
+      categories: categories,
+      data: data
+    }
+  }
+})
