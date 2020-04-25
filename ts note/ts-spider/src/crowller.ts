b9e650b6d2fafc23432872596accc8f152239afa
Lac <laclys@126.com>
2020-04-25 22:06:43 +0800

    optimize

--- ts note/ts-spider/src/crowller.ts
+++ ts note/ts-spider/src/crowller.ts
@@ -20,6 +20,7 @@ interface Content {
 class Crowller {
   private secret = 'x3b174jsx';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+  private filePath = path.resolve(__dirname, '../data/course.json');
 
   getCoureInfo(html: string) {
     const $ = cheerio.load(html);
@@ -46,21 +47,23 @@ class Crowller {
   }
 
   generateJsonContent(courseInfo: CourseRet) {
-    const filePath = path.resolve(__dirname, '../data/course.json');
     let fileContent: Content = {};
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    if (fs.existsSync(this.filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
     }
     fileContent[courseInfo.time] = courseInfo.data;
     return fileContent
   }
 
+  writeFile (content: string) {
+    fs.writeFileSync(this.filePath, 'utf-8')
+  }
+
   async initSpiderProcess() {
-    const filePath = path.resolve(__dirname, '../data/course.json');
     const html = await this.getRawHtml();
     const courseInfo = this.getCoureInfo(html);
     const fileContent = this.generateJsonContent(courseInfo);
-    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+    this.writeFile(JSON.stringify(fileContent))
   }
 
   constructor() {
