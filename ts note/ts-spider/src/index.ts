f292aa9af27947b74aa14e4ed7337ddd05eba777
Lac <laclys@126.com>
2020-06-03 00:41:59 +0800

    added

--- ts note/ts-spider/src/index.ts
+++ ts note/ts-spider/src/index.ts
@@ -1,7 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
 import router from './router';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.nnname = 'Lac';
+  next();
+});
 app.use(router);
 
 app.listen(7001, () => [console.log('server is running')]);
--- ts note/ts-spider/src/router.ts
+++ ts note/ts-spider/src/router.ts
@@ -2,18 +2,38 @@ import { Router, Request, Response } from 'express';
 import Crowller from './crowller';
 import Analyzer from './analyzer';
 
+interface RequestWithBody extends Request {
+  body: {
+    [key: string]: string | undefined;
+  };
+}
+
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('hello word');
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password" />
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
-router.get('/getData', (req: Request, res: Response) => {
-  const secret = 'x3b174jsx';
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-  const analyzer = Analyzer.getInstance();
-  new Crowller(url, analyzer);
-  res.send('get data success!');
+router.get('/getData', (req: RequestWithBody, res: Response) => {
+  const { password } = req.body;
+  if (password === '123') {
+    const secret = 'x3b174jsx';
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+    const analyzer = Analyzer.getInstance();
+    new Crowller(url, analyzer);
+    res.send('get data success!');
+  } else {
+    res.send('password Error!');
+  }
 });
 
 export default router;
